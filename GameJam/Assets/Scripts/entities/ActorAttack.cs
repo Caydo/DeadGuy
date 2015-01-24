861e8888e484075fc55394f6511083f63004b2ae
Caydo <directorjoe@gmail.com>
2015-01-24 15:58:42 -0700

    Adding prefabs and test scene.

--- GameJam/Assets/Scripts/entities/ActorAttack.cs
+++ GameJam/Assets/Scripts/entities/ActorAttack.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ActorAttack : MonoBehaviour {
+public class ActorAttack : MonoBehaviour
+{
+  public GameObject AttackOnePrefab;
+  public GameObject AttackTwoPrefab;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  public Transform AttackOnePosition;
+  public Transform AttackTwoPosition;
+  
+  void Update()
+  {
+    if(Input.GetButton("Fire1"))
+    {
+      // attack one
+      GameObject.Instantiate(AttackOnePrefab, AttackOnePosition.position, Quaternion.identity);
+    }
+
+    if(Input.GetButton("Fire2"))
+    {
+      // attack two
+      GameObject.Instantiate(AttackTwoPrefab, AttackOnePosition.position, Quaternion.identity);
+    }
+  }
 }
---
+++ GameJam/Assets/Scripts/entities/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+  public bool IsRanged;
+  public float killTime;
+  bool fired = false;
+  float forceAmount = 100;
+
+	void OnEnable()
+  {
+    if(!fired)
+    {
+      StartCoroutine(waitThenDestroy());
+      fired = true;
+
+      if(IsRanged)
+      {
+        Vector3 movementForce = new Vector3(0, forceAmount, 0);
+        rigidbody.AddForce(movementForce);
+      }
+    }
+  }
+
+  IEnumerator waitThenDestroy()
+  {
+    yield return new WaitForSeconds(killTime);
+    GameObject.Destroy(gameObject);
+  }
+}
