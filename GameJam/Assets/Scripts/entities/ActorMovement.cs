50926b93fc0fe91e930a1a8507602a5569d0fa09
Caydo <directorjoe@gmail.com>
2015-01-24 15:03:20 -0700

    Removing DemonEnemy.cs since it'll just use Actor to get data. Fixing VS compile error.

---
+++ GameJam/Assets/Scripts/entities/ActorMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorMovement : MonoBehaviour
+{
+  public bool IsPlayer;
+  
+  void Update()
+  {
+    if(IsPlayer)
+    {
+      if(Input.GetKeyUp(KeyCode.UpArrow))
+      {
+        // go up
+      }
+      
+      if(Input.GetKeyUp(KeyCode.DownArrow))
+      {
+        // go down
+      }
+      
+      if(Input.GetKeyUp(KeyCode.LeftArrow))
+      {
+        // go left
+      }
+      
+      if(Input.GetKeyUp(KeyCode.RightArrow))
+      {
+        // go right
+      }
+    }
+    else
+    {
+      // look for player
+      // if player is in range of attack then attempt to attack, otherwise move toward the player and then try to attack again, rinse and repeat
+    }
+  }
+}
