a80e94e2d66570f33f7175bbcd960468a51a8f61
kaitlyn culley <kaitlyn.culley@gmail.com>
2015-01-24 21:40:57 -0700

    add particles, fonts and enemy prefab

---
+++ GameJam/Assets/Scripts/DeleteAfterTimeout.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteAfterTimeout : MonoBehaviour {
+	public float lifespan = 5.0f;
+
+	void Start() {
+		StartCoroutine(Delete());
+	}
+
+	IEnumerator Delete() {
+		yield return new WaitForSeconds(lifespan);
+		Destroy(gameObject);
+	}
+}
