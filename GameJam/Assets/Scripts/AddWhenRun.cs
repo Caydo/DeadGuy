aafeb03dbeeabe35b3c39c62567d14c8c3038fec
kaitlyn culley <kaitlyn.culley@gmail.com>
2015-01-25 15:46:07 -0700

    ROTATE YO SHADOW

---
+++ GameJam/Assets/Scripts/AddWhenRun.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddWhenRun : MonoBehaviour
+{
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		var instance = GameObject.Instantiate (prefab) as GameObject;
+		instance.transform.parent = transform;
+		instance.transform.rotation = Quaternion.identity;
+		instance.transform.localPosition = Vector3.back;
+	}
+}
---
+++ GameJam/Assets/Scripts/entities/LookPlayerRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookPlayerRotate : MonoBehaviour
+{
+	GameObject player;
+
+	float spriteOffset = 30f;
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void LateUpdate (){
+		transform.rotation =// Quaternion.Lerp(
+			//transform.rotation,
+			Quaternion.LookRotation(transform.position, player.transform.position);
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z+spriteOffset);
+	}
+}
